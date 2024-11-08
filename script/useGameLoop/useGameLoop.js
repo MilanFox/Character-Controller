d2fbbca92987aec210cde42137a217d5fc02f6fe
Milan Fox <milan.fox@diva-e.com>
2024-11-08 09:04:43 +0100

    add performance indicator

--- script/useGameLoop/useGameLoop.js
+++ script/useGameLoop/useGameLoop.js
@@ -3,11 +3,18 @@ import { entities } from '../useEntity/useEntity.js';
 import { ambientEffects } from '../useTerrain/useTerrainGeneration.js';
 import { player } from '../usePlayer/usePlayer.js';
 
+let lastFrameTime = performance.now();
+let fps = 0;
+
 const drawFrame = () => {
+  const currentFrameTime = performance.now();
+  fps = Math.round(1000 / (currentFrameTime - lastFrameTime));
+  lastFrameTime = currentFrameTime;
   entityCanvas.getContext('2d').clearRect(0, 0, entityCanvas.width, entityCanvas.height);
   backgroundAmbientCanvas.getContext('2d').clearRect(0, 0, entityCanvas.width, entityCanvas.height);
   drawEntity(player, entityCanvas);
   ambientEffects.background.entities.forEach(effectEntity => drawEntity(effectEntity, backgroundAmbientCanvas));
+  entityCanvas.getContext('2d').fillText(`FPS: ${fps}`, 10, 20);
 };
 
 export const runGameLoop = () => {
